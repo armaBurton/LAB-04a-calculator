477853ecad3b6d7cbfbf42a1c77fb3e345f4cfad
armaBurton <armanarma@armaburton.com>
2021-10-07 11:15:24 -0700

    ran test functions

--- test/tests.js
+++ test/tests.js
@@ -3,15 +3,37 @@
 
 const test = QUnit.test;
 
+function add(x, y){
+    return(x + y);
+};
+
 // name your test by what it is testing
-test('time to test a function', (expect) => {
+test('add 5 + 3', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 5;
+    const y = 3;
+    const expected = 8;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = add(x, y); // use your function here
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('add 4 + 3', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const x = 4;
+    const y = 3;
+    const expected = 7;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(x, y); // use your function here
 
     //Expect
     // Make assertions about what is expected versus the actual result
