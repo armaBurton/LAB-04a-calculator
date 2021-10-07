9905d9dbe9ab19433708fb80b78bcf2c5d371513
armaBurton <armanarma@armaburton.com>
2021-10-07 16:15:33 -0700

    added space after 'return' to pass npx eslint

--- calculations.js
+++ calculations.js
@@ -1,39 +1,39 @@
 export function add(x, y){
-    return(x + y);
+    return (x + y);
 }
 
 export function subtract(x, y){
-    return(x - y);
+    return (x - y);
 }
 
 export function multiply(x, y){
-    return(x * y);
+    return (x * y);
 }
 
 export function divide(x, y){
-    return(x / y);
+    return (x / y);
 }
 
 export function modulo(x, y){
-    return(x % y);
+    return (x % y);
 }
 
 export function integerDivision(x, y){
-    return(Math.floor(x / y));
+    return (Math.floor(x / y));
 }
 
 export function hypotenuse(x, y){
-    return(Math.sqrt((x * x) + (y * y)));
+    return (Math.sqrt((x * x) + (y * y)));
 }
 
 export function volOfSphere(x){
-    return((4 / 3) * Math.PI * Math.pow(x, 3));
+    return ((4 / 3) * Math.PI * Math.pow(x, 3));
 }
 
 export function volOfCylinder(r, h){
-    return(Math.PI * Math.pow(r, 2) * h);
+    return (Math.PI * Math.pow(r, 2) * h);
 }
 
 export function areaOfTriangle(x, y){
-    return((x * y) / 2);
+    return ((x * y) / 2);
 }
